763a091a84ce346a4fcb85c3419dfafa38bf33a1
huyanping <hypxm@qq.com>
2014-08-22 14:23:33 +0800

    modify System V Message Queue

--- core/SystemVMessageQueue.class.php
+++ core/SystemVMessageQueue.class.php
@@ -8,7 +8,8 @@
  * System V message queue IPC通信消息队列封装
  */
 
-class SystemVMessageQueue implements IMessageQueue {
+class SystemVMessageQueue implements IMessageQueue
+{
 
     //消息分组类型，用于将一个消息队列中的信息进行分组
     private $msg_type;
@@ -39,7 +40,8 @@ class SystemVMessageQueue implements IMessageQueue {
      * @param int $option_receive 设置位MSG_IPC_NOWAIT，如果无法获取到一个消息，则不等待；如果设置位NULL，则会等待消息到来
      * @param int $maxsize 希望接收到的最大消息
      */
-    public function __construct($msg_type, $ipc_filename=__FILE__, $serialize_needed=true, $block_send=false, $option_receive=MSG_IPC_NOWAIT, $maxsize=100000){
+    public function __construct($msg_type, $ipc_filename = __FILE__, $serialize_needed = true, $block_send = false, $option_receive = MSG_IPC_NOWAIT, $maxsize = 100000)
+    {
         $this->msg_type = $msg_type;
         $this->serialize_needed = $serialize_needed;
         $this->block_send = $block_send;
@@ -48,38 +50,60 @@ class SystemVMessageQueue implements IMessageQueue {
         $this->init_queue($ipc_filename, $msg_type);
     }
 
-    public function init_queue($ipc_filename, $msg_type){
+    /**
+     * 初始化一个队列
+     * @param $ipc_filename
+     * @param $msg_type
+     * @throws Exception
+     */
+    public function init_queue($ipc_filename, $msg_type)
+    {
         $this->key_t = $this->get_ipc_key($ipc_filename, $msg_type);
         $this->queue = msg_get_queue($this->key_t);
-        if(!$this->queue) throw new Exception('msg_get_queue failed');
+        if (!$this->queue) throw new Exception('msg_get_queue failed');
     }
 
-    public function get_ipc_key($ipc_filename, $msg_type){
+    /**
+     * @param $ipc_filename
+     * @param $msg_type
+     * @return int
+     * @throws Exception
+     */
+    public function get_ipc_key($ipc_filename, $msg_type)
+    {
         $key_t = ftok($ipc_filename, $msg_type);
-        if($key_t<=0) throw new Exception('ftok error');
+        if ($key_t == 0) throw new Exception('ftok error');
 
         return $key_t;
     }
 
-    public function get(){
+    /**
+     * 从队列获取一个
+     * @return bool
+     * @throws Exception
+     */
+    public function get()
+    {
         $queue_status = $this->status();
-        if ($queue_status['msg_qnum']>0) {
-            if (msg_receive($this->queue,$this->msg_type ,$msgtype_erhalten,$this->maxsize,$data,$this->serialize_needed, $this->option_receive, $err)===true) {
+        if ($queue_status['msg_qnum'] > 0) {
+            if (msg_receive($this->queue, $this->msg_type, $msgtype_erhalten, $this->maxsize, $data, $this->serialize_needed, $this->option_receive, $err) === true) {
                 return $data;
             } else {
                 throw new Exception($err);
             }
-        }else{
+        } else {
             return false;
         }
     }
 
     /**
+     * 写入队列
      * @param $message
      * @throws Exception
      */
-    public function put($message){
-        if(!msg_send($this->queue,$this->msg_type, $message,$this->serialize_needed, $this->block_send,$err)===true){
+    public function put($message)
+    {
+        if (!msg_send($this->queue, $this->msg_type, $message, $this->serialize_needed, $this->block_send, $err) === true) {
             throw new Exception($err);
         }
 
@@ -99,36 +123,90 @@ class SystemVMessageQueue implements IMessageQueue {
      *               On Linux, this value may be read and modified via /proc/sys/kernel/msgmnb.
      * msg_lspid	 The pid of the process that sent the last message to the queue. 最后发送消息的进程ID
      * msg_lrpid	 The pid of the process that received the last message from the queue. 最后接收消息的进程ID
+     *
+     * @return array
      */
-    public function status(){
+    public function status()
+    {
         $queue_status = msg_stat_queue($this->queue);
         return $queue_status;
     }
 
-    public function size(){
+    /**
+     * 获取队列当前堆积状态
+     * @return mixed
+     */
+    public function size()
+    {
         $status = $this->status();
         return $status['msg_qnum'];
     }
 
 
     /**
-     * 修改队列状态信息，下标可以是status_queue返回值中下标的任意一个
+     * allows you to change the values of the msg_perm.uid,
+     * msg_perm.gid, msg_perm.mode and msg_qbytes fields of the underlying message queue data structure
      * 可以用来修改队列运行接收的最大读取的数据
+     *
      * @param $key 状态下标
      * @param $value 状态值
      * @return bool
      */
-    public function set_status($key, $value){
-        $queue_status = $this->status_queue();
+    public function set_status($key, $value)
+    {
+        $this->check_set_privilege($key);
+
+        if ($key == 'msg_qbytes')
+            return $this->set_max_queue_size($value);
+
         $queue_status[$key] = $value;
         return msg_set_queue($this->queue, $queue_status);
     }
 
-    public function queue_remove(){
+    /**
+     * 删除一个队列
+     * @return bool
+     */
+    public function queue_remove()
+    {
         return msg_remove_queue($this->queue);
     }
 
-    public function queue_exists($key){
+    //修改队列能容纳的最大字节数，需要root权限
+    /**
+     * @param $size
+     * @return bool
+     * @throws Exception
+     */
+    public function set_max_queue_size($size)
+    {
+        $user = get_current_user();
+        if ($user !== 'root')
+            throw new Exception('changing msg_qbytes needs root privileges');
+
+        return $this->set_status('msg_qbytes', $size);
+    }
+
+    /**
+     * 判断一个队列是否存在
+     * @param $key
+     * @return bool
+     */
+    public function queue_exists($key)
+    {
         return msg_queue_exists($key);
+    }
+
+    /**
+     * 检查修改队列状态的权限
+     * @param $key
+     * @throws Exception
+     */
+    private function check_set_privilege($key)
+    {
+        $privilege_field = array('msg_perm.uid', 'msg_perm.gid', 'msg_perm.mode');
+        if (!in_array($key, $privilege_field)) {
+            throw new Exception('you can only change msg_perm.uid, msg_perm.gid, msg_perm.mode and msg_qbytes. And msg_qbytes needs root privileges');
+        }
     }
 } 
