6f30906b9b8f9e508058c1db4675a38b6a3a167e
huyanping <hypxm@qq.com>
2014-08-28 19:55:23 +0800

    add default construct param

--- core/RedisMessageQueue.class.php
+++ core/RedisMessageQueue.class.php
@@ -8,7 +8,8 @@
  * 基于Redis的消息队列封装
  */
 
-class RedisMessageQueue implements IMessageQueue {
+class RedisMessageQueue implements IMessageQueue
+{
 
     private $redis_server;
 
@@ -27,8 +28,9 @@ class RedisMessageQueue implements IMessageQueue {
      * @param $key
      * @param bool $p_connect
      */
-    public function __construct($server_config, $key, $p_connect=false){
-        if(empty($key))
+    public function __construct($server_config = array('IP' => '127.0.0.1', 'PORT' => '6379'), $key = 'redis_message_queue', $p_connect = false)
+    {
+        if (empty($key))
             throw new Exception('message queue key can not be empty');
 
         $this->server = $server_config['IP'];
@@ -36,9 +38,9 @@ class RedisMessageQueue implements IMessageQueue {
         $this->key = $key;
 
         $this->check_environment();
-        if($p_connect){
+        if ($p_connect) {
             $this->pconnect();
-        }else{
+        } else {
             $this->connect();
         }
     }
@@ -46,14 +48,16 @@ class RedisMessageQueue implements IMessageQueue {
     /**
      * 析构函数，关闭redis链接，使用长连接时，最好主动调用关闭
      */
-    public function __destruct(){
+    public function __destruct()
+    {
         $this->close();
     }
 
     /**
      * 短连接
      */
-    private function connect(){
+    private function connect()
+    {
         $this->redis_server = new Redis();
         $this->redis_server->connect($this->server, $this->port);
     }
@@ -61,7 +65,8 @@ class RedisMessageQueue implements IMessageQueue {
     /**
      * 长连接
      */
-    public function pconnect(){
+    public function pconnect()
+    {
         $this->redis_server = new Redis();
         $this->redis_server->pconnect($this->server, $this->port);
     }
@@ -69,7 +74,8 @@ class RedisMessageQueue implements IMessageQueue {
     /**
      * 关闭链接
      */
-    public function close(){
+    public function close()
+    {
         $this->redis_server->close();
     }
 
@@ -78,7 +84,8 @@ class RedisMessageQueue implements IMessageQueue {
      * @param $message
      * @return mixed
      */
-    public function put($message){
+    public function put($message)
+    {
         return $this->redis_server->lPush($this->key, $message);
     }
 
@@ -86,7 +93,8 @@ class RedisMessageQueue implements IMessageQueue {
      * 从队列顶部获取一条记录
      * @return mixed
      */
-    public function get(){
+    public function get()
+    {
         return $this->redis_server->lPop($this->key);
     }
 
@@ -94,7 +102,8 @@ class RedisMessageQueue implements IMessageQueue {
      * 选择数据库，可以用于区分不同队列
      * @param $database
      */
-    public function select($database){
+    public function select($database)
+    {
         $this->redis_server->select($database);
     }
 
@@ -102,7 +111,8 @@ class RedisMessageQueue implements IMessageQueue {
      * 获得队列状态，即目前队列中的消息数量
      * @return mixed
      */
-    public function size(){
+    public function size()
+    {
         return $this->redis_server->lSize($this->key);
     }
 
@@ -111,7 +121,8 @@ class RedisMessageQueue implements IMessageQueue {
      * @param $pos
      * @return mixed
      */
-    public function view($pos){
+    public function view($pos)
+    {
         return $this->redis_server->lGet($this->key, $pos);
     }
 
@@ -119,7 +130,8 @@ class RedisMessageQueue implements IMessageQueue {
      * 检查Redis扩展
      * @throws Exception
      */
-    private function check_environment(){
+    private function check_environment()
+    {
         if (!extension_loaded('redis')) {
             throw new Exception('Redis extension not loaded');
         }
