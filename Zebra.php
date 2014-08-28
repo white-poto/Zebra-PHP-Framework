b9d332c470bfafc41c907da2b88b42d2bc4e7b84
huyanping <hypxm@qq.com>
2014-08-28 20:17:50 +0800

    add Zebra.php

--- Zebra.php
+++ Zebra.php
@@ -11,12 +11,14 @@ define('DS', DIRECTORY_SEPARATOR);
 
 function __autoload($class_name) {
     $class_file = ROOT . DS . 'core' . DS . $class_name . '.class.php';
+    echo $class_file . PHP_EOL;
     if(file_exists($class_name)){
         require_once $class_file;
         return true;
     }
 
     $interface_file = ROOT . DS . 'core' . DS . $class_name . '.interface.php';
+    echo $interface_file . PHP_EOL;
     if(file_exists($interface_file)){
         require_once $interface_file;
         return true;
