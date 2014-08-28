31341c1584b19f258c1e501e6b75779bb9589579
huyanping <hypxm@qq.com>
2014-08-28 20:58:13 +0800

    modify RunningTime

--- core/RunningTime.class.php
+++ core/RunningTime.class.php
@@ -4,26 +4,61 @@
  * User: huyanping
  * Date: 14-8-28
  * Time: 下午8:42
+ *
+ * PHP脚本运行计时器
+ * 调用方法：
+ * $runningTime = new RunningTime();
+ * $runningTime->begin();
+ * sleep(10);
+ * $runningTime->mark('test');
+ * $runningTime->end();
+ * echo $runningTime->getReport();
+ *
  */
 
 class RunningTime {
 
+    /**
+     * 运行时间记录器
+     */
     private $times;
 
+    /**
+     * 程序开始计时
+     */
     public function begin(){
         $this->times['begin_time'] = $this->getCurrentTime();
     }
 
+    /**
+     * 程序结束计时
+     */
     public function end(){
         $this->times['end_time'] = $this->getCurrentTime();
     }
 
+    /**
+     * 程序中间运行计时
+     * @param $key 运行标志，例如一个函数名称
+     */
     public function mark($key){
         $this->times[$key] = $this->getCurrentTime();
     }
 
+    /**
+     * 获取运行计时器
+     * @return mixed
+     */
+    public function getTimes(){
+        return $this->times;
+    }
+
+    /**
+     * 获取运行报告
+     * @return string
+     */
     public function getReport(){
-        $report = '';
+        $report = 'Running Time Report:' . PHP_EOL;
 
         $pre_time = -1;
         foreach($this->times as $key=>$time){
@@ -39,6 +74,10 @@ class RunningTime {
         return $report;
     }
 
+    /**
+     * 获取当前时间
+     * @return float
+     */
     public function getCurrentTime ()
     {
         list ($msec, $sec) = explode(" ", microtime());
