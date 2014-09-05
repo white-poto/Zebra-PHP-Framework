bf6940ebee34e7efcc691b7a9df3b31abe4e3783
huyanping <hypxm@qq.com>
2014-09-05 14:51:37 +0800

    add redis_message_queue_status_test

--- tests/MessageQueue/redis_message_queue_status_test.php
+++ tests/MessageQueue/redis_message_queue_status_test.php
@@ -7,3 +7,10 @@
  */
 
 require '../../Zebra.php';
+
+$message = new \Zebra\MessageQueue\RedisMessageQueueStatus();
+for($i=0; $i<100; $i++){
+    $message->put(mt_rand(0, 10000));
+}
+
+echo $message->status_normal();
