f687839cbc32fd352f0a407eb7cdf68363dae4a4
huyanping <hypxm@qq.com>
2014-10-04 09:19:00 +0800

    modify SingleObject.class.php

--- Zebra/Object/SingleObject.class.php
+++ Zebra/Object/SingleObject.class.php
@@ -6,6 +6,7 @@
  * Time: 下午12:17
  *
  * 单例对象创建工具
+ * $object = SingleObject::getInstance('Class_Name');
  */
 
 namespace Zebra\Object;
@@ -15,7 +16,7 @@ class SingleObject {
 
     private static $object;
 
-    public static function make($class_name){
+    public static function getInstance($class_name){
         if(isset(self::$object[$class_name]) && is_object(self::$object[$class_name])) {
             self::$object[$class_name] = new $class_name;
         }
