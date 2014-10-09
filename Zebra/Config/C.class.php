da0149be9095bc7453a9eb784ed32284b9c2ff3e
huyanping <hypxm@qq.com>
2014-10-09 22:34:39 +0800

    update C.class.php

--- Zebra/Config/C.class.php
+++ Zebra/Config/C.class.php
@@ -15,68 +15,63 @@ namespace Zebra\Config;
 
 class C {
 
-    private static $config;
-
-    private function __construct(){}
+    protected static $config;
 
     /**
      * 根据配置文件名称、配置key，获取配置值
      * @param $key
-     * @param string $configFile
      * @return bool
      */
-    public static function get($key, $configFile='config'){
-        self::initConfig($configFile);
-        if(!isset(self::$config[$configFile][$key])){
-            return false;
-        }
-        return self::$config[$configFile][$key];
-    }
+    public static function get($key){
+        self::initConfig($key);
+        $keyInfo = explode('.', $key);
+        $configFile = $keyInfo[0];
+        $realKey = $keyInfo[1];
 
-    /**
-     * 根据配置文件名称、配置key、字段名称，获取配置值
-     * @param $key
-     * @param $field
-     * @param string $configFile
-     * @return bool
-     */
-    public function getField($key, $field, $configFile='config'){
-        self::initConfig($configFile);
-        if(!isset(self::$config[$configFile][$key][$field])){
-            return false;
+        if(isset($keyInfo[2]) && !empty($keyInfo[2])){
+            $fieldName = $keyInfo[2];
+            return self::$config[$configFile][$realKey][$fieldName];
+        }else{
+            return self::$config[$configFile][$realKey];
         }
-        return self::$config[$configFile][$key][$field];
+
+        return false;
     }
 
     /**
      * 根据配置文件名称，配置key，设置配置值，不提供持久化，程序退出后即失效
      * @param $key
      * @param $value
-     * @param string $configFile
+     * @throws \Exception
      */
-    public function set($key, $value, $configFile='config'){
-        self::$config[$configFile][$key] = $value;
-    }
+    public static function set($key, $value){
+        if(!\strstr($key, '.')) throw new \Exception('param key does not contain "."');
+        $keyInfo = explode('.', $key);
+        $configFile = $keyInfo[0];
+        $realKey = $keyInfo[1];
 
-    /**
-     * 根据配置文件名称、配置key，字段名称设置配置值，不提供持久化，程序退出后即失效
-     * @param $key
-     * @param $field
-     * @param $value
-     * @param string $configFile
-     */
-    public function setField($key, $field, $value, $configFile='config'){
-        self::$config[$configFile][$key][$field] = $value;
+        if(isset($keyInfo[2]) && !empty($keyInfo[2])){
+            $fieldName = $keyInfo[2];
+            self::$config[$configFile][$realKey][$fieldName] = $value;
+        }else{
+            self::$config[$configFile][$realKey] = $value;
+        }
     }
 
     /**
      * 单例模式初始化配置文件
-     * @param string $configFile
+     * @param string $key
      * @throws \Exception
      */
-    private static function initConfig($configFile='config'){
+    private static function initConfig($key){
+        if(!\defined('CONFIG_PATH')) throw new \Exception('const CONFIG_PATH is not defined');
+
+        if(!\strstr($key, '.')) throw new \Exception('param key does not contain "."');
+        $keyInfo = explode('.', $key);
+        $configFile = $keyInfo[0];
+
         if(!isset(self::$config[$configFile])) {
-            $realConfigFile = CONFIG_PATH . DS . $configFile . '.ini.php';;
+            $realConfigFile = CONFIG_PATH . DIRECTORY_SEPARATOR . $configFile . '.php';;
             if(!file_exists($realConfigFile)) throw new \Exception('config file is not exists');
             if(!is_readable($realConfigFile)) throw new \Exception('config file is not readable');
             if(!checkPhpSyntax($realConfigFile)) throw new \Exception('config file contains a syntax error');
